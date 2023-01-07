dd2149bd8c7038ff6541d4e922391ebf208360cd
Sumit Das <sumit8das@gmail.com>
2023-01-07 11:38:46 +0530

    Security Group Rule

--- security_group_rule.tf
+++ security_group_rule.tf
@@ -21,3 +21,14 @@ resource "aws_security_group_rule" "tls_in" {
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
+
+
+resource "aws_security_group_rule" "http_in" {
+    protocol = "tcp"
+    security_group_id = aws_security_group.my_security_group.id
+    from_port = 80
+    to_port = 80
+    type = "ingress"
+    cidr_blocks = ["0.0.0.0/0"]
+}
